466bd5cacccc481d669e6e8e861bee4d8e7c4ceb
Emerson Silva <emerson_jdss@hotmail.com>
2024-09-13 13:53:13 -0300

    adjust graph

--- src/app/graph/page.tsx
+++ src/app/graph/page.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import React, { useEffect, useRef, useState } from "react";
 import cytoscape from "cytoscape";
 import Modal from "react-modal";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 
 const GraphWithModal = () => {
   const cyRef = useRef<cytoscape.Core | null>(null);
@@ -12,16 +16,58 @@ const GraphWithModal = () => {
       container: document.getElementById("cy"), // Contêiner HTML onde o grafo será renderizado
 
       elements: [
-        // Define nós e arestas do grafo
+        // Nó A e suas conexões
         { data: { id: "a", label: "API A" } },
         { data: { id: "b", label: "API B" } },
         { data: { id: "c", label: "API C" } },
-        { data: { source: "a", target: "b" } },
-        { data: { source: "b", target: "c" } },
+        { data: { source: "b", target: "a" } },
+        { data: { source: "c", target: "a" } },
+
+        // Nó D e suas conexões, com o nó G em estado de erro
+        { data: { id: "d", label: "API D" } },
+        { data: { id: "e", label: "API E" } },
+        { data: { id: "f", label: "API F" } },
+        { data: { id: "g", label: "API G", error: true } }, // Nó com erro
+        { data: { id: "h", label: "API H" } },
+        { data: { source: "e", target: "d" } },
+        { data: { source: "f", target: "d" } },
+        { data: { source: "g", target: "d" } },
+        { data: { source: "h", target: "d" } },
       ],
 
+      //   style: [
+      //     // Estilo dos nós e arestas
+      //     {
+      //       selector: "node",
+      //       style: {
+      //         "background-color": "#007bff",
+      //         label: "data(label)",
+      //         "text-valign": "center",
+      //         "text-halign": "center",
+      //         color: "#fff",
+      //         width: 80,
+      //         height: 80,
+      //       },
+      //     },
+      //     {
+      //       selector: "edge",
+      //       style: {
+      //         width: 2,
+      //         "line-color": "#ccc",
+      //         "target-arrow-color": "#ccc",
+      //         "target-arrow-shape": "triangle",
+      //       },
+      //     },
+      //   ],
+
+      //   layout: {
+      //     name: "grid",
+      //     rows: 1,
+      //   },
+      // });
+
       style: [
-        // Estilo dos nós e arestas
+        // Estilo dos nós
         {
           selector: "node",
           style: {
@@ -30,8 +76,24 @@ const GraphWithModal = () => {
             "text-valign": "center",
             "text-halign": "center",
             color: "#fff",
+            height: 80,
+            width: 80,
           },
         },
+        // Estilo do nó com erro
+        {
+          selector: "node[error]",
+          style: {
+            "background-color": "#ef4444", // Fundo vermelho para o nó com erro
+            label: "data(label)",
+            "text-valign": "center",
+            "text-halign": "center",
+            color: "#fff",
+            height: 80,
+            width: 80,
+          },
+        },
+        // Estilo das arestas
         {
           selector: "edge",
           style: {
@@ -45,7 +107,7 @@ const GraphWithModal = () => {
 
       layout: {
         name: "grid",
-        rows: 1,
+        rows: 2,
       },
     });
 
@@ -64,19 +126,27 @@ const GraphWithModal = () => {
   };
 
   return (
-    <div>
-      <div id="cy" style={{ width: "600px", height: "400px" }}></div>
+    <>
+      <DefaultLayout>
+        <div className="mx-auto max-w-242.5">
+          <Breadcrumb pageName="Graph" />
 
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        contentLabel="Node Information"
-      >
-        <h2>Instruções para {selectedNode}</h2>
-        <p>Essas são as instruções para a {selectedNode}.</p>
-        <button onClick={closeModal}>Fechar</button>
-      </Modal>
-    </div>
+          <div>
+            <div id="cy" className="mx-auto flex flex-1 bg-orange-500"></div>
+
+            <Modal
+              isOpen={modalIsOpen}
+              onRequestClose={closeModal}
+              contentLabel="Node Information"
+            >
+              <h2>Instruções para {selectedNode}</h2>
+              <p>Essas são as instruções para a {selectedNode}.</p>
+              <button onClick={closeModal}>Fechar</button>
+            </Modal>
+          </div>
+        </div>
+      </DefaultLayout>
+    </>
   );
 };
 
